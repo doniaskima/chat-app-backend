4214cccaf90e5be5b5494ff4c84a10f826af8c89
unknown <doniaskima344@gmail.com>
2022-10-22 10:12:35 +0100

    group.model is fine

---
+++ models/group.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const groupSchema = new mongoose.Schema({
+    admin: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    description: mongoose.Schema.Types.String,
+    isPublic: mongoose.Schema.Types.Boolean,
+    groupCode: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    member: [{
+        typetype: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }],
+    name: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+
+const Group = mongoose.model("groups", groupSchema);
+
+module.exports = Group;
