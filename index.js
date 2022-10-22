32888b813af5eac2119d0df031e176c5c7af0d96
unknown <doniaskima344@gmail.com>
2022-10-22 09:13:57 +0100

    index done

---
+++ index.js
@@ -0,0 +1,33 @@
+//import section
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+
+//DB connection
+mongoose.connect(process.env.MONGO_DB_URI);
+mongoose.connection.on("connected", () => {
+    console.log("DB connected");
+});
+mongoose.connection.on("error", (err) => {
+    console.log("mongodb failed with", err);
+});
+//import routes
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+//routes middleware
+
+const port = 8000;
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
