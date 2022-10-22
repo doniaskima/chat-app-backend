e1f8d3c9219d8d4bcc4cf9aa89500b1f49f3d6ff
unknown <doniaskima344@gmail.com>
2022-10-22 19:00:07 +0100

    fnct encrypt added

---
+++ controllers/message.controller.js
@@ -0,0 +1,20 @@
+const Message = require("../models/user.model");
+const crypto = require("crypto");
+const User = require("../models/user.model");
+
+const encrypt = (message) => {
+    // key to encrypt and decrypted  (random 32 Bytes)
+    const key = crypto.randomBytes(32);
+    //iv - initialization vector (random 16 Bytes)
+    const iv = crypto.randomBytes(16);
+    // cipher function to encrypt the message
+    // aes-256-cbc algorithm to encrypt and decrypt the data.
+    let cipher = crypto.createCipheriv("aes-256-cbc", Buffer.from(key), iv);
+    let encryptedMessage = cipher.update(message);
+    encryptedMessage = Buffer.concat([encryptedMessage, cipher.final()]);
+    return {
+        iv: iv.toString("hex"),
+        encryptedMessage: encryptedMessage.toString("hex"),
+        key: key.toString("hex"),
+    };
+};
