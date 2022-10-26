ee3fdca0e2efa9df7e5542e58c8d3f02bee9bbe0
unknown <doniaskima344@gmail.com>
2022-10-26 20:42:59 +0100

    fix the createGroup rout

---
+++ routes/group.route.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const {
+    createGroup,
+
+} = require("../controllers/group.controller");
+
+
+router.route("/create").post(createGroup);
