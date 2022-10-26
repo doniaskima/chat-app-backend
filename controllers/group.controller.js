0e61fd97038dbf0a4116113a918b6f27feeb27ee
unknown <doniaskima344@gmail.com>
2022-10-26 20:26:52 +0100

    gonna add createGroup

---
+++ controllers/group.controller.js
@@ -0,0 +1,9 @@
+const Group = require("../models/group.model");
+const Message = require("../models/message.model");
+const User = require("../models/user.model");
+
+const randomCode = () => Math.floor(Math.random() * (1000 - 1) + 1);
+
+const createGroup = async(req, res) => {
+
+}
