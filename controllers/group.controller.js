d327d5da5a811da8ed2fe2fc9e078f29948dd4c2
unknown <doniaskima344@gmail.com>
2022-10-26 20:41:25 +0100

    working on createGroup

--- controllers/group.controller.js
+++ controllers/group.controller.js
@@ -5,5 +5,30 @@ const User = require("../models/user.model");
 const randomCode = () => Math.floor(Math.random() * (1000 - 1) + 1);
 
 const createGroup = async(req, res) => {
+    const { adminId, groupName, isPublic, description } = req.body;
+    const user = await User.findById(adminId);
+    if (user) {
+        const newGroup = new Group({
+            name: groupName,
+            admin: adminId,
+            groupCode: randomCode(),
+            isPublic: isPublic,
+            description: description,
+        });
+        newGroup.members.push(user._id);
+        const groupInfo = await newGroup.save();
+        user.groups.push(groupInfo._id);
+        await user.save();
+        return res.json({
+            status: true,
+            message: "Group created",
+            group: groupInfo,
+        })
+    }
+    return res.json({ status: false, message: "user not found" });
+}
 
+
+module.exports = {
+    createGroup,
 }
