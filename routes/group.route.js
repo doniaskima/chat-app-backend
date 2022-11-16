d21a14ee58d7c76faadcfc73067babb419ce59df
unknown <doniaskima344@gmail.com>
2022-11-16 01:15:44 +0100

    fix the route

--- routes/group.route.js
+++ routes/group.route.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const {
     createGroup,
+    updateGroup,
 
 } = require("../controllers/group.controller");
 
 
 router.route("/create").post(createGroup);
-router.route("/")
+router.route("/update_group").put(updateGroup);
 
 module.exports = router;
