8fc791ee2b3f0c0ba55d7ccf04ddfbe80d6fc4c9
unknown <doniaskima344@gmail.com>
2022-11-19 18:36:32 +0100

    signup route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
 
 import './App.css';
-import { Login } from "./pages/index";
 import { Router } from "react-router-dom";
+import { Home, Login, Signup } from "./pages/index";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Login path="/" />
+        <Home path="home" />
+        <Signup path="signup" />
       </Router>
     </div>
   );
+
 }
+
 
 export default App;
