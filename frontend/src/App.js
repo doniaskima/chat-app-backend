a4cac77fdaccd973962b9a8c340a984260c4731f
unknown <doniaskima344@gmail.com>
2022-11-19 18:52:21 +0100

    feat:privat route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@
 import './App.css';
 import { Router } from "react-router-dom";
 import { Home, Login, Signup } from "./pages/index";
+import { PrivateRoute } from "./components/PrivateRoute";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Login path="/" />
-        <Home path="home" />
+        <PrivateRoute path="home" />
         <Signup path="signup" />
       </Router>
     </div>
