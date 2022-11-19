62b28a83e5a996d7fd3907ba1bdcfccbf97f5774
unknown <doniaskima344@gmail.com>
2022-11-19 18:31:06 +0100

    feat: reach router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 
 import './App.css';
+import { Login } from "./pages/index";
+import { Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Login path="/" />
+      </Router>
     </div>
   );
 }
--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,12 +1,11 @@
-import React from 'react'
-
-export const Input = ({callback,placeholder,value,type}) => {
+export const Input = ({ callback, placeholder, value, type }) => {
   return (
-      <input
-          type={type} 
-          placeholder={placeholder}
-          value={value}
-          onChange="max-w-60  md:w-72 bg-transparent border-2 px-3 py-2 outlune-none border-black rounded-md"
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={callback}
+      className="max-w-60 md:w-72 bg-transparent border-2 px-3 py-2 outline-none border-black rounded-md"
     />
-  )
-}
+  );
+};
